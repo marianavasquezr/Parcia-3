79e9a3585e58ccb860b354aaee49c5abd8466b1f
estefania <estefania.loaizas@udea.edu.co>
2024-05-13 01:45:56 -0500

    v2: realice metodos de la clase dicomfile como el de extraer la info de los pacientes de los archivos dicom, tambien el de cargar la imagen jpg o png, realice esto en el archivo de clases y de implementacion. Ademas adjunte carpetas con archivos dicom e imagenes jpg y png, tambien cree una carpea para guardar los archivos dicom rotados y una para los nifti

--- clases.py
+++ clases.py
@@ -1,7 +1,53 @@
-import matplotlib as plt 
+import matplotlib.pyplot as plt 
 import cv2 
 import pydicom 
 import os
+import numpy as np 
+import nibabel as nib
 
 class DicomFile:
-    pass 
+    def __init__(self, nombre, edad, ID, imagen_nifti):
+        self.nombre = nombre
+        self.edad = edad
+        self.ID = ID
+        self.imagen = imagen_nifti
+        
+    def extraer_info_paciente_dicom(ruta_dicom):
+        try:
+            dicom_data = pydicom.dcmread(ruta_dicom)
+            nombre = dicom_data.PatientName
+            edad = dicom_data.PatientAge
+            ID = dicom_data.PatientID
+            return {"Nombre": nombre, "Edad": edad, "ID": ID}
+        
+        except Exception as e:
+            print(f"Error al procesar el archivo DICOM {ruta_dicom}: {e}")
+            return None
+
+    def extraer_info_pacientes(carpeta_dicom):
+        pacientes = []
+
+        # Iterar sobre todos los archivos en la carpeta DICOM
+        for raiz, _, archivos in os.walk(carpeta_dicom):
+            for archivo in archivos:
+                if archivo.endswith('.dcm'):
+                    ruta_dicom = os.path.join(raiz, archivo)
+                    paciente = DicomFile.extraer_info_paciente_dicom(ruta_dicom)
+                    if paciente:
+                        pacientes.append(paciente)
+
+        return pacientes
+
+class ImagenFile:
+    def __init__(self, ruta):
+        self.ruta = ruta
+
+    def cargar_imagen(self):
+        imagen = cv2.imread(self.ruta)
+        return imagen
+
+    def guardar_imagen(self, imagen, ruta_destino):
+        cv2.imwrite(ruta_destino, imagen)
+
+    def binarizacion_morfologia(self, umbral, tamano_kernel):
+        pass
--- implementacion.py
+++ implementacion.py
@@ -1,9 +1,15 @@
-from clases import DicomFile
+from clases import DicomFile, ImagenFile
+import matplotlib as plt 
+import cv2 
+import pydicom 
+import os
+import numpy as np 
+import nibabel as nib
 pacientes = {}
 archivos = {}
 
 while True:
-    print("\nMenú Principal:")
+    print("\n----------------Menú Principal:--------------------")
     print("1. Ingresar Paciente (DICOM)")
     print("2. Ingresar Imagen (JPG/PNG)")
     print("3. Realizar Transformación de Rotación")
@@ -13,16 +19,39 @@ while True:
 
     if opcion == "1":
         # Implementa la opción para ingresar pacientes con archivos DICOM
-        pass
+        # Ruta de la carpeta que contiene los archivos DICOM
+        carpeta_dicom = input("Ingrese la ruta de la carpeta de archivos dicom: ")
+
+        # Extraer información de los pacientes
+        pacientes = DicomFile.extraer_info_pacientes(carpeta_dicom)
+
+        # Mostrar información de los pacientes
+        for idx, paciente in enumerate(pacientes, start=1):
+            print(f"Paciente {idx}:")
+            print(f"Nombre: {paciente['Nombre']}")
+            print(f"Edad: {paciente['Edad']}")
+            print(f"ID: {paciente['ID']}")
+            print("-" * 30)
         
     elif opcion == "2":
         # Implementa la opción para ingresar imágenes JPG o PNG
-        pass
+        ruta_imagen = input("Ingrese la ruta de la imagen (JPG/PNG): ")
+        if os.path.exists(ruta_imagen):
+            archivos[ruta_imagen] = ImagenFile(ruta_imagen)
+            print("---------------------------")
+            print("Imagen ingresada con éxito.")
+            print("---------------------------")
+            
+        else:
+                print("------------------------------------------------------")
+                print("La ruta de la imagen no es válida. Inténtelo de nuevo.")
+                print("------------------------------------------------------")
         
     elif opcion == "3":
         # Implementa la opción para realizar la transformación de rotación
         pass
-        
+
+    
     elif opcion == "4":
         # Implementa la opción para realizar binarización y transformación morfológica
         pass
